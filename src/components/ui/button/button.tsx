7e7c8412a9780d2b6212a1f743587bf92f3d7ff4
ksasrg <ksahtmlcss@gmail.com>
2023-06-26 16:01:49 +0300

    button

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -14,6 +14,9 @@ export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) 
   const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
 
   return (
-    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
+    <Component
+      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      {...rest}
+    />
   )
 }
